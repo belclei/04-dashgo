594bcca26e10bdd06064a9927138294f8426e8c8
belclei <belclei@gmail.com>
2021-10-11 23:52:21 -0300

    pages updates

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,62 @@
+import { Box, Flex, Button, Heading, Icon, Table, Thead, Tbody, Tr, Th, Td, Checkbox, Text } from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination'
+import { Sidebar } from '../../components/Sidebar'
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Lista de usuários
+            </Heading>
+            <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiAddLine} />}>
+              Criar Novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" w="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Belclei Fasoolo</Text>
+                    <Text fontSize="sm" color="gray.300">
+                      belclei@gmail.com
+                    </Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>05 de Outubro de 2018</Text>
+                </Td>
+                <Td>
+                  <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon as={RiPencilLine} />}>
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
